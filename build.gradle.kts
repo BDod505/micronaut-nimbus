bc4d8cc8048f6db550a8fc5b74d1073da4a6f5bf
Balwant Dod <balwant.dod@mnscorp.net>
2024-12-20 11:54:36 +0000

    initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.4.2"
+    id("io.micronaut.aot") version "4.4.2"
+}
+
+version = "0.1"
+group = "com.micronaut.nimbus"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor("io.micronaut:micronaut-http-validation")
+    implementation("io.micronaut:micronaut-jackson-databind")
+    compileOnly("io.micronaut:micronaut-http-client")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    testImplementation("io.micronaut:micronaut-http-client")
+}
+
+
+application {
+    mainClass = "com.micronaut.nimbus.MicronautNimbus"
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+    targetCompatibility = JavaVersion.toVersion("21")
+}
+
+
+graalvmNative.toolchainDetection = false
+
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("com.micronaut.nimbus.*")
+    }
+    aot {
+        optimizeServiceLoading = false
+        convertYamlToJava = false
+        precomputeOperations = true
+        cacheEnvironment = true
+        optimizeClassLoading = true
+        deduceEnvironment = true
+        optimizeNetty = true
+        replaceLogbackXml = true
+    }
+}
+
+
+tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
+    jdkVersion = "21"
+}
+
+
---
+++ src/main/java/com/micronaut/nimbus/MicronauNimbus.java
@@ -0,0 +1,10 @@
+package com.micronaut.nimbus;
+
+import io.micronaut.runtime.Micronaut;
+
+public class MicronauNimbus {
+
+    public static void main(String[] args) {
+        Micronaut.run(MicronauNimbus.class, args);
+    }
+}
